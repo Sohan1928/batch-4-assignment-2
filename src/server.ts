5d85fce6ccd45d1cc006a36e18e152e1ebdbc4c5
Sohan1928 <sohanurrahman1928@gmail.com>
2024-11-24 09:43:37 +0600

    mongodb atlas connect done

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,15 @@
+import mongoose from "mongoose";
 import app from "./app";
+import config from "./app/config";
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+main();
